8c01d29eea6e1254c989dc53326a7d902a16fa76
pradipantu <pradipantu@hotmail.com>
2021-02-25 22:35:35 +0530

    [-] RestCall with order easy service

--- src/main/java/com/client/springcloudconfigclient/ClientController.java
+++ src/main/java/com/client/springcloudconfigclient/ClientController.java
@@ -4,7 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,4 +25,12 @@ public class ClientController {
         logger.info("External Properties :{}",externalServerProperties);
         return externalServerProperties.toString();
     }
+
+    @PostMapping(path="/post",consumes = "application/json",produces = "application/json")
+    public ResponseEntity<String> postDetails(@RequestBody String data){
+        logger.info("Received request :{}",data);
+        return new ResponseEntity<>(data, HttpStatus.CREATED);
+    }
+
+
 }
